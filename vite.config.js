b0fd979c6f8c139c6721a194e6d1dded09c38c3e
osama <osama11111777@gmail.com>
2025-08-08 22:05:25 +0300

    Fix Vite base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Movies-Project/', // اسم الريبو بين سلاشات
+  base: '/', // اسم الريبو بين سلاشات
 })
